6b2f2fc4f41c4d241e261bd510855c588c304874
Hosein Kiarsy Pour <kiarsy@yahoo.com>
2021-02-01 14:29:38 +0330

    x

--- tun2socks.go
+++ tun2socks.go
@@ -1,53 +1,63 @@
-package tun2socks
+package v2rayCore
 
 import (
-	"context"
-	"runtime"
-	"runtime/debug"
-	"strings"
-	"time"
+	"log"
 
-	"github.com/eycorsican/go-tun2socks/common/log"
-	"github.com/eycorsican/go-tun2socks/core"
-	"github.com/eycorsican/go-tun2socks/proxy/v2ray"
 	vcore "v2ray.com/core"
-	vproxyman "v2ray.com/core/app/proxyman"
+
+	// The following are necessary as they register handlers in their init functions.
+	// Required features. Can't remove unless there is replacements.
+	_ "v2ray.com/core/app/dispatcher"
+	_ "v2ray.com/core/app/proxyman/inbound"
+	_ "v2ray.com/core/app/proxyman/outbound"
+
+	// Other optional features.
+	_ "v2ray.com/core/app/dns"
+	_ "v2ray.com/core/app/log"
+	_ "v2ray.com/core/app/policy"
+	_ "v2ray.com/core/app/router"
+	_ "v2ray.com/core/app/stats"
+
+	// Inbound and outbound proxies.
+	_ "v2ray.com/core/proxy/blackhole"
+	_ "v2ray.com/core/proxy/dokodemo"
+	_ "v2ray.com/core/proxy/freedom"
+	_ "v2ray.com/core/proxy/http"
+	_ "v2ray.com/core/proxy/mtproto"
+	_ "v2ray.com/core/proxy/shadowsocks"
+	_ "v2ray.com/core/proxy/socks"
+	_ "v2ray.com/core/proxy/vmess/inbound"
+	_ "v2ray.com/core/proxy/vmess/outbound"
+
+	// Transports
+	_ "v2ray.com/core/transport/internet/domainsocket"
+	_ "v2ray.com/core/transport/internet/http"
+	_ "v2ray.com/core/transport/internet/kcp"
+	_ "v2ray.com/core/transport/internet/quic"
+	_ "v2ray.com/core/transport/internet/tcp"
+	_ "v2ray.com/core/transport/internet/tls"
+	_ "v2ray.com/core/transport/internet/udp"
+	_ "v2ray.com/core/transport/internet/websocket"
+
+	// Transport headers
+	_ "v2ray.com/core/transport/internet/headers/http"
+	_ "v2ray.com/core/transport/internet/headers/noop"
+	_ "v2ray.com/core/transport/internet/headers/srtp"
+	_ "v2ray.com/core/transport/internet/headers/tls"
+	_ "v2ray.com/core/transport/internet/headers/utp"
+	_ "v2ray.com/core/transport/internet/headers/wechat"
+	_ "v2ray.com/core/transport/internet/headers/wireguard"
+
+	// The following line loads JSON internally
+	_ "v2ray.com/core/main/jsonem"
 )
 
-type PacketFlow interface {
-	WritePacket(packet []byte)
-}
-
-func InputPacket(data []byte) {
-	lwipStack.Write(data)
-}
-
-var lwipStack core.LWIPStack
-
-func StartV2Ray(packetFlow PacketFlow, configBytes []byte) {
-	if packetFlow == nil {
-		return
-	}
-
-	lwipStack = core.NewLWIPStack()
-	v, err := vcore.StartInstance("json", configBytes)
+func StartWithJsonData(configBytes []byte) bool {
+	_, err := vcore.StartInstance("json", configBytes)
 	if err != nil {
 		log.Fatalf("start V instance failed: %v", err)
+		return false
 	}
 
-	sniffingConfig := &vproxyman.SniffingConfig{
-		Enabled:             true,
-		DestinationOverride: strings.Split("tls,http", ","),
-	}
-
-	debug.SetGCPercent(5)
-	ctx := vproxyman.ContextWithSniffingConfig(context.Background(), sniffingConfig)
-	core.RegisterTCPConnHandler(v2ray.NewTCPHandler(ctx, v))
-	core.RegisterUDPConnHandler(v2ray.NewUDPHandler(ctx, v, 30*time.Second))
-	core.RegisterOutputFn(func(data []byte) (int, error) {
-		packetFlow.WritePacket(data)
-		runtime.GC()
-		debug.FreeOSMemory()
-		return len(data), nil
-	})
+	return true
 }
